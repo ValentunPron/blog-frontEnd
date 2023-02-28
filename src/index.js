a22704c77e61ecaa856ec37ecb240069eba0d3c2
Valentyn <valentunpron@gmail.com>
2023-02-28 15:42:31 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Provider } from "react-redux";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter, HashRouter } from "react-router-dom"
 import App from "./App";
 import CssBaseline from "@mui/material/CssBaseline";
 
@@ -16,11 +16,11 @@ root.render(
   <>
     <CssBaseline />
     <ThemeProvider theme={theme}>
-      <BrowserRouter>
+      <HashRouter>
         <Provider store={store}>
           <App />
         </Provider>
-      </BrowserRouter>
+      </HashRouter>
     </ThemeProvider>
   </>
 );

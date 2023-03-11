980fc0e5e406f142abc48cf773fe4c1a510c495c
Valentyn <valentunpron@gmail.com>
2023-03-11 10:38:35 +0200

    fix corse

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const instance = axios.create({
 	baseURL: process.env.REACT_APP_API_URL,
-	'Content-Type': 'text/plain'
 });
 
 instance.interceptors.request.use((config) => {
